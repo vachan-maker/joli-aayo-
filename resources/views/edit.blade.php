4051c3a69064c4c154124619aa58b3a415520023
vachan-maker <vachan@tutanota.com>
2026-06-19 17:05:59 +0530

    Modified dropdown and make it work properly

--- resources/views/edit.blade.php
+++ resources/views/edit.blade.php
@@ -4,15 +4,14 @@
                 @method('PUT')
         <input type="text" name="company_name" placeholder="Company Name" value="{{ old('company_name', $application->company_name) }}"/>
         <input type="text" name="role_title" placeholder="Role Title" value="{{ old('company_name', $application->role_title) }}"/>
-        <select>
-            <option value="applied">Applied</option>
-            <option value="interview">Interview</option>
-            <option value="rejected">Rejected</option>
-            <option value="offer">Offer</option>
-            <option value="accepted">Accepted</option>
-            <option value="gd">Group Discussion</option>
-            <option value="test/aptitude">Test</option>
-        </select>
+            @foreach(App\Enums\ApplicationStatus::cases() as $status)
+        <option
+            value="{{ $status->value }}"
+            {{ old('status') === $status->value ? 'selected' : '' }}
+        >
+            {{ $status->value }}
+        </option>
+            @endforeach
         <input type="date" name="date_applied" value="{{ old('date', $application->date_applied) }}"/>
         <input type="url" name="job_url" placeholder="job-url" value="{{ old('job_url', $application->job_url) }}"/>
         <input type="email" name="email" placeholder="email" value="{{ old('email', $application->email) }}"/>
