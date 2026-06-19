ff89e42f6219af0fd3f501e932c0284ec8c63dab
vachan-maker <vachan@tutanota.com>
2026-06-19 16:42:46 +0530

    Dynamically generate the dropdown list using enums

--- resources/views/create.blade.php
+++ resources/views/create.blade.php
@@ -1,32 +1,30 @@
 <x-layout>
     <form action="{{ route('applications.store') }}" method="post">
         @csrf
-                @if ($errors->any())
-        <aside>
-            <strong>Please correct the errors below:</strong>
-            <ul>
-                @foreach ($errors->all() as $error)
-                    <li>{{ $error }}</li>
-                @endforeach
-            </ul>
-        </aside>
-    @endif
-        <input type="text" name="company_name" placeholder="Company Name"/>
-        <input type="text" name="role_title" placeholder="Role Title"/>
-        <select>
-            <option value="applied">Applied</option>
-            <option value="interview">Interview</option>
-            <option value="rejected">Rejected</option>
-            <option value="offer">Offer</option>
-            <option value="accepted">Accepted</option>
-            <option value="gd">Group Discussion</option>
-            <option value="test/aptitude">Test</option>
+        @if ($errors->any())
+            <aside>
+                <strong>Please correct the errors below:</strong>
+                <ul>
+                    @foreach ($errors->all() as $error)
+                        <li>{{ $error }}</li>
+                    @endforeach
+                </ul>
+            </aside>
+        @endif
+        <input type="text" name="company_name" placeholder="Company Name" />
+        <input type="text" name="role_title" placeholder="Role Title" />
+        <select name="status">
+            @foreach(App\Enums\ApplicationStatus::cases() as $status)
+                <option value="{{ $status->value }}">
+                    {{ $status->value }}
+                </option>
+            @endforeach
         </select>
-        <input type="date" name="date_applied" max="{{ date('Y-m-d') }}"/>
-        <input type="url" name="job_url" placeholder="job-url"/>
-        <input type="email" name="email" placeholder="email"/>
-        <input type="string" name="source" placeholder="source"/>
-        <input type="submit"/>
+        <input type="date" name="date_applied" max="{{ date('Y-m-d') }}" value="{{ date('Y-m-d') }}" />
+        <input type="url" name="job_url" placeholder="job-url" />
+        <input type="email" name="email" placeholder="email" />
+        <input type="string" name="source" placeholder="source" />
+        <input type="submit" />
 
     </form>
 </x-layout>
