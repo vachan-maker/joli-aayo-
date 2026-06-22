cee3402d5910631a575b1d461200a4909e962fb0
vachan-maker <vachan@tutanota.com>
2026-06-22 16:53:55 +0530

    Added feature to view the resume being used for a particular application

--- resources/views/resume/show.blade.php
+++ resources/views/resume/show.blade.php
@@ -3,21 +3,36 @@
         <header>
             <h1>{{ $resume->label }}</h1>
         </header>
-
         <section>
-            <iframe src="{{ route('resume.view',$resume ->id)}}" width="100%" height=800></iframe>
+            <h3>Applications using this Resume</h3>
+            <ul>
+                @foreach($resume->applications as $application)
+                    <li>
+                        <a href="{{ route('applications.show', $application->id) }}">
+                        {{ $application->company_name }}
+                        -
+                        {{ $application->role_title }}
+                    </a>
+                    </li>
+                @endforeach
+            </ul>
+        </section>
+        <section>
+            <iframe src="{{ route('resume.view', $resume->id)}}" width="100%" height=800></iframe>
         </section>
 
         <footer>
-                    <footer>
-            <form action="{{ route('resume.destroy', $resume->id) }}" method="post">
-                @csrf
-                @method('DELETE')
-                <input type="submit" value="Delete" onclick="return confirm('Are you sure you want to delete this application?')"/>
-                <a href="{{ route('resume.download',$resume) }}"><input type="button" value="Download" class="outline"/></a>
-            </form>
-            
-        </footer>
+            <footer>
+                <form action="{{ route('resume.destroy', $resume->id) }}" method="post">
+                    @csrf
+                    @method('DELETE')
+                    <input type="submit" value="Delete"
+                        onclick="return confirm('Are you sure you want to delete this application?')" />
+                    <a href="{{ route('resume.download', $resume) }}"><input type="button" value="Download"
+                            class="outline" /></a>
+                </form>
+
+            </footer>
         </footer>
     </article>
 </x-layout>
