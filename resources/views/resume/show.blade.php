b8f1976137e736f9ad9aed9b9f88b26a57019187
vachan-maker <vachan@tutanota.com>
2026-06-22 17:02:32 +0530

    Use an if condition to check it

--- resources/views/resume/show.blade.php
+++ resources/views/resume/show.blade.php
@@ -4,18 +4,22 @@
             <h1>{{ $resume->label }}</h1>
         </header>
         <section>
-            <h3>Applications using this Resume</h3>
-            <ol>
-                @foreach($resume->applications as $application)
-                    <li>
-                        <a href="{{ route('applications.show', $application->id) }}">
-                        {{ $application->company_name }}
-                        -
-                        {{ $application->role_title }}
-                    </a>
-                    </li>
-                @endforeach
-            </ol>
+            {{-- check if there are applications associated with the resume. If not don't display it  --}}
+            @if ($resume->applications  && $resume->applications->isNotEmpty())
+                <h3>Applications using this Resume</h3>
+                <ol>
+                    @foreach($resume->applications as $application)
+                        <li>
+                            <a href="{{ route('applications.show', $application->id) }}">
+                                {{ $application->company_name }}
+                                -
+                                {{ $application->role_title }}
+                            </a>
+                        </li>
+                    @endforeach
+                </ol>
+
+            @endif
         </section>
         <section>
             <iframe src="{{ route('resume.view', $resume->id)}}" width="100%" height=800></iframe>
