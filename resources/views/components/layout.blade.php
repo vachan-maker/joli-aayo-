63f7d4b199925557f3d5ab4f4291b0131a58e57c
vachan-maker <vachan@tutanota.com>
2026-06-24 09:46:41 +0530

    intialised font-awesome

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -6,6 +6,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@picocss/pico@2/css/pico.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/7.0.1/css/all.min.css">
     <title>{{ $title }}</title>
 </head>
 
@@ -19,11 +20,15 @@
             <ul>
                 <li>
                     @if (request()->routeIs('applications.*'))
-                        <a href="{{ route('applications.create') }}">Add Application</a>
+                        <a href="{{ route('applications.create') }}">
+                            <i class="fa-solid fa-plus"></i>
+                           
+                            Add Application
+                        </a>
                     @endif
                     @if (request()->routeIs('resume.*'))
-                        <a href="{{ route('resume.create') }}">Add Resume</a>
-                        
+                        <a href="{{ route('resume.create') }}"> <i class="fa-solid fa-plus"></i>Add Resume</a>
+                       
                     @endif
                 </li>
                 @auth
